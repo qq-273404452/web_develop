6728afa340f26e6011301fe76536ae3c2b83be4b
Rubicon_Chen <273404452@qq.com>
2020-03-06 22:32:41 +0800

    socket-io
    realtime example

--- user.py
+++ user.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from flask import Flask, Blueprint, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Never tell you!'
+socketio = SocketIO(app)
 
 bp=Blueprint('user', __name__, url_prefix='/user')
 
-@bp.route('/')
+@app.route('/')
 def index():
-    return 'hello world'
+    return render_template('base.html')
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message:' + message)
+
+if __name__ == '__main__':
+    socketio.run(app,host='127.0.0.1',port='8080',debug=True)
